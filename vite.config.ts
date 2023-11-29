84362ce6a2ac2cbab349fc1fe8c43d361430d447
Taku345 <takuma432@gmail.com>
2023-11-29 11:18:09 +0900

    続き

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     proxy: {
       // APIのエンドポイントを指定し、CORSを有効にする
       '/api': {
-        target: 'http://127.0.0.1:8000', // APIのURLを指定
+        // target: 'http://127.0.0.1:8000', // APIのURLを指定
+        target: 'https://www.takuma432.shop', // APIのURLを指定
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
