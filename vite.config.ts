6b6fbd399574a8da60e0605f4aa0799c43c6b37e
Taku345 <takuma432@gmail.com>
2023-11-29 19:08:29 +0900

    なんとかなれーっ！

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,15 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      // APIのエンドポイントを指定し、CORSを有効にする
-      '/api': {
-        // target: 'http://127.0.0.1:8000', // APIのURLを指定
-        target: 'https://www.takuma432.shop', // APIのURLを指定
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+    // proxy: {
+    //   // APIのエンドポイントを指定し、CORSを有効にする
+    //   '/api': {
+    //     // target: 'http://127.0.0.1:8000', // APIのURLを指定
+    //     target: 'https://www.takuma432.shop', // APIのURLを指定
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ''),
+    //   },
+    // },
     hmr: {
       host: 'https://www.takuma432.shop',
     },
